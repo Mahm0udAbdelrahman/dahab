5cb8fe915c50ad21f0e8e1323a3b240df4d165a2
Mahm0udAbdelrahman <mahmudabdelrahman0@gmail.com>
2026-03-07 19:54:22 +0200

    Dahab

--- resources/views/dashboard/pages/auth/login.blade.php
+++ resources/views/dashboard/pages/auth/login.blade.php
@@ -4,7 +4,7 @@
 <!-- [Head] start -->
 
 <head>
-    <title>Login | Travel</title>
+    <title>Login | Dahab</title>
     <!-- [Meta] -->
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui" />
